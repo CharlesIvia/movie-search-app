c92148698eb17ced4f05a0a98d93d0ec228dea12
Charles Ivia <iviacharles@gmail.com>
2020-06-04 09:57:51 +0300

    Initial styling

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>Hooked</h1>
+      </header>
     </div>
   );
 }
