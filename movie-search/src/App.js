5af872023917e3fb1b40b4aeccb050965a50e321
Charles Ivia <iviacharles@gmail.com>
2020-06-04 08:24:14 +0300

    Created and intergrated the Movie.js component

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Movie from "./components/Movie";
 
 function App() {
   return (
     <div className="App">
-      <Header text="Hooked"/>
+      <Header text="Hooked" />
+      <Movie />
     </div>
   );
 }
