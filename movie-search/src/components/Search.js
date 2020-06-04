b35ce82fa6a7568c11587c6adcf9e572e66ecaa0
Charles Ivia <iviacharles@gmail.com>
2020-06-04 10:12:31 +0300

    Created search component

---
+++ movie-search/src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search() {
+    return (
+        <section className="searchbox-wrap">
+            <input type="text" placeholder="Search for a movie..." className="searchbox" autoFocus/>
+        </section>
+    )
+}
+
+export default Search
