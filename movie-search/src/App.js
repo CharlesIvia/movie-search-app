8e53f7d8cd4c73c3901742720f2fe00d2169b297
Charles Ivia <iviacharles@gmail.com>
2020-06-04 08:39:30 +0300

    Created and intergrated search component

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Movie from "./components/Movie";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <Header text="Hooked" />
       <Movie />
+      <Search />
     </div>
   );
 }
